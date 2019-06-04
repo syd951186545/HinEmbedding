eee777babd0256627dfdadfd563b9d0e4ce9f7fd
SYD <951186545@qq.com>
2019-06-05 00:47:51 +0800

    fix

--- utils/HinData2EdgeFile.py
+++ utils/HinData2EdgeFile.py
@@ -8,9 +8,10 @@ def hinData2edgeFile(datasetFolder, edgelistFile, labelFile):
     """
     将异质网络数据转换为同质网络数据并以.edgelist的格式存储，同时存储.label文件
 
-    :param datasetFolder: 异质网络数据集所在的文件夹，其中文件后缀表示文件类别（边文件.edge，标签文件.label）
-    :param edgelistFile:转换后的edgelist文件路径
-    :param labelFile:转换后的label文件路径
+    :param datasetFolder: 异质网络数据集所在的@！文件夹路径！例如 "1_DBLP"
+    文件夹中文件名称无所谓，文件后缀需满足../data中的规范，即（边文件.edge，标签文件.label，名称映射文件.map）
+    :param edgelistFile:转换后的edgelist存储文件路径
+    :param labelFile:转换后的label存储文件路径
     :return:None
     """
     folder = dir + datasetFolder
